f604e0de0b498ac674a108d9b3813ae215fa8a84
krevetka <krevetka@DESKTOP-6LBMUL2>
2023-06-23 14:38:35 +0300

    1

---
+++ Home6/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()=> $"{Title} by {Author}";
+       
+    }
+
+    class BookList
+    {
+        private List<Book> books = new List<Book>();
+
+        public void Add(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void Remove(Book book)
+        {
+            books.Remove(book);
+        }
+
+        public bool Contains(Book book)
+        {
+            return books.Contains(book);
+        }
+
+        public Book this[int index]
+        {
+            get
+            {
+                return books[index];
+            }
+            set
+            {
+                books[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return books.Count;
+            }
+        }
+    }
+}
--- Home6/Program.cs
+++ Home6/Program.cs
@@ -10,7 +10,23 @@ namespace Home6
     {
         static void Main(string[] args)
         {
+            BookList myBooks = new BookList();
 
+            // добавляем книги в список
+            myBooks.Add(new Book { Title = "The Catcher in the Rye", Author = "J.D. Salinger" });
+            myBooks.Add(new Book { Title = "To Kill a Mockingbird", Author = "Harper Lee" });
+            myBooks.Add(new Book { Title = "1984", Author = "George Orwell" });
+            
+            // удаляем книгу из списка
+            myBooks.Remove(new Book { Title = "To Kill a Mockingbird", Author = "Harper Lee" });
+
+            // проверяем наличие книги в списке
+            Console.WriteLine(myBooks.Contains(new Book { Title = "1984", Author = "George Orwell" })); 
+            Console.WriteLine(myBooks.Contains(new Book { Title = "The Great Gatsby", Author = "F. Scott Fitzgerald" })); 
+
+            // получаем книгу по индексу
+            Console.WriteLine(myBooks[0]); // The Catcher in the Rye by J.D. Salinger
         }
     }
+    
 }
