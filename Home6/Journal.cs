b1cee689b20989f66302b5def07319f9cf7130a1
krevetka <krevetka@DESKTOP-6LBMUL2>
2023-06-23 14:30:41 +0300

    Добавьте файлы проекта.

---
+++ Home6/Journal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    internal class Journal
+    {
+        private int _numEmploye;
+
+        public int NumEmploye
+        {
+            get { return _numEmploye; }
+            set { _numEmploye = value; }
+        }
+
+        public static Journal operator +(Journal journal, int value)
+        {
+            journal.NumEmploye += value;
+            return journal;
+        }
+
+        public static Journal operator -(Journal journal, int value)
+        {
+            journal.NumEmploye -= value;
+            return journal;
+        }
+
+        public static bool operator ==(Journal journal1, Journal journal2)
+        {
+            if (ReferenceEquals(journal1, journal2))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(journal1, null) || ReferenceEquals(journal2, null))
+            {
+                return false;
+            }
+
+            return journal1.NumEmploye == journal2.NumEmploye;
+        }
+
+        public static bool operator !=(Journal journal1, Journal journal2)
+        {
+            return !(journal1 == journal2);
+        }
+
+        public static bool operator <(Journal journal1, Journal journal2)
+        {
+            if (ReferenceEquals(journal1, null))
+            {
+                return !ReferenceEquals(journal2, null);
+            }
+
+            return journal1.NumEmploye < journal2.NumEmploye;
+        }
+
+        public static bool operator >(Journal journal1, Journal journal2)
+        {
+            if (ReferenceEquals(journal2, null))
+            {
+                return !ReferenceEquals(journal1, null);
+            }
+
+            return journal1.NumEmploye > journal2.NumEmploye;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Journal;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.NumEmploye == other.NumEmploye;
+        }
+    }
+}
---
+++ Home6/Shop.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    internal class Shop
+    {
+        private double _area;
+
+        public double Area
+        {
+            get { return _area; }
+            set { _area = value; }
+        }
+
+        public static Shop operator +(Shop shop, double value)
+        {
+            shop.Area += value;
+            return shop;
+        }
+
+        public static Shop operator -(Shop shop, double value)
+        {
+            shop.Area -= value;
+            return shop;
+        }
+
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            if (ReferenceEquals(shop1, shop2))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(shop1, null) || ReferenceEquals(shop2, null))
+            {
+                return false;
+            }
+
+            return shop1.Area == shop2.Area;
+        }
+
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return !(shop1 == shop2);
+        }
+
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            if (ReferenceEquals(shop1, null))
+            {
+                return !ReferenceEquals(shop2, null);
+            }
+
+            return shop1.Area < shop2.Area;
+        }
+
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            if (ReferenceEquals(shop2, null))
+            {
+                return !ReferenceEquals(shop1, null);
+            }
+
+            return shop1.Area > shop2.Area;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Shop;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Area == other.Area;
+        }
+    }
+}
